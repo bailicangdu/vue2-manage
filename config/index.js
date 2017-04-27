3c1a34ef9381b16b8b5a072e1a1a1c5a15f03bdb
maguohua <1264889788@qq.com>
2017-04-27 17:55:14 +0800

    change port

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8080,
+    port: 8002,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
